ea6ed79526284f77ae5c3ba0e8dbe70387efb360
h-saadatmand <132128338+h-saadatmand@users.noreply.github.com>
2026-09-13 09:43:05 +1000

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 
 from torch.nn import functional as F
 
-'''
+
 def get_loss_module(class_weights=None):
     return NoFussCrossEntropyLoss(weight=class_weights, reduction='none')
 
@@ -24,51 +24,6 @@ class NoFussCrossEntropyLoss(nn.CrossEntropyLoss):
         return F.cross_entropy(inp, target.long(), weight=self.weight,
                                ignore_index=self.ignore_index, reduction=self.reduction)
 
-'''
 
 
 
-def get_loss_module():
-    
-    
-        return NoFussCrossEntropyLoss(reduction='none')  # outputs loss for each batch sample
-
-
-def l2_reg_loss(model):
-    """Returns the squared L2 norm of output layer of given model"""
-
-    for name, param in model.named_parameters():
-        if name == 'output_layer.weight':
-            return torch.sum(torch.square(param))
-
-
-class NoFussCrossEntropyLoss(nn.CrossEntropyLoss):
-    """
-    pytorch's CrossEntropyLoss is fussy: 1) needs Long (int64) targets only, and 2) only 1D.
-    This function satisfies these requirements
-    """
-
-    def forward(self, inp, target):
-        return F.cross_entropy(inp, target.long(), weight=self.weight,
-                               ignore_index=self.ignore_index, reduction=self.reduction)
-# def get_loss_module():
-#         return NoFussCrossEntropyLoss(reduction='none')  # outputs loss for each batch sample
-
-
-# def l2_reg_loss(model):
-#     """Returns the squared L2 norm of output layer of given model"""
-
-#     for name, param in model.named_parameters():
-#         if name == 'output_layer.weight':
-#             return torch.sum(torch.square(param))
-
-
-# class NoFussCrossEntropyLoss(nn.CrossEntropyLoss):
-#     """
-#     pytorch's CrossEntropyLoss is fussy: 1) needs Long (int64) targets only, and 2) only 1D.
-#     This function satisfies these requirements
-#     """
-
-#     def forward(self, inp, target):
-#         return F.cross_entropy(inp, target.long(), weight=self.weight,
-#                                ignore_index=self.ignore_index, reduction=self.reduction)
